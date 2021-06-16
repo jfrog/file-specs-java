9ebf09f6bd0a1f6844fe5b78b82522c831b4e358
Asaf Gabai <asafg@jfrog.com>
2021-06-16 09:07:48 +0300

    Fixed tests

--- src/test/java/org/jfrog/filespecs/DistributionHelperTest.java
+++ src/test/java/org/jfrog/filespecs/DistributionHelperTest.java
@@ -24,7 +24,7 @@ public class DistributionHelperTest {
                 "{\"files\": [{\"pattern\": \"repo-local/file.sfx\"}]}",
                 Collections.singletonList(
                         new DistributionSpecComponent(
-                                "items.find({\"$or\":[{\"$and\":[{\"repo\":\"repo-local\",\"path\":\".\",\"name\":\"file.sfx\"}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")",
+                                "items.find({\"$or\":[{\"$and\":[{\"repo\":\"repo-local\",\"path\":\".\",\"name\":\"file.sfx\"}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")",
                                 new ArrayList<>(),
                                 new ArrayList<>())
                 )
@@ -32,7 +32,7 @@ public class DistributionHelperTest {
                 "{\"files\": [{\"pattern\": \"a/b/c\", \"target\": \"a/b/x\", \"targetProps\": \"k1=v11,v12;k2=v2;k1=v13,v11\"}]}",
                 Collections.singletonList(
                         new DistributionSpecComponent(
-                                "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"a\",\"path\":\"b\",\"name\":\"c\"}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")",
+                                "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"a\",\"path\":\"b\",\"name\":\"c\"}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")",
                                 Collections.singletonList(new PathMapping("^a/b/c$", "a/b/x")),
                                 Arrays.asList(new Property("k1", asSet("v11", "v12", "v13")), new Property("k2", asSet("v2"))))
                 )
@@ -40,11 +40,11 @@ public class DistributionHelperTest {
                 "{\"files\": [{\"pattern\": \"a/*/c/*\", \"target\": \"a/b/c/{1}-{2}\"}, {\"pattern\": \"a/b/c\", \"target\": \"a/b/x\"}]}",
                 Arrays.asList(
                         new DistributionSpecComponent(
-                                "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"a\",\"path\":{\"$match\":\"*/c\"},\"name\":{\"$match\":\"*\"}}]},{\"$and\":[{\"repo\":\"a\",\"path\":{\"$match\":\"*/c/*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")",
+                                "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"a\",\"path\":{\"$match\":\"*/c\"},\"name\":{\"$match\":\"*\"}}]},{\"$and\":[{\"repo\":\"a\",\"path\":{\"$match\":\"*/c/*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")",
                                 Collections.singletonList(new PathMapping("^a/.*/c/.*$", "a/b/c/$1-$2")),
                                 new ArrayList<>()),
                         new DistributionSpecComponent(
-                                "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"a\",\"path\":\"b\",\"name\":\"c\"}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")",
+                                "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"a\",\"path\":\"b\",\"name\":\"c\"}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")",
                                 Collections.singletonList(new PathMapping("^a/b/c$", "a/b/x")),
                                 new ArrayList<>())
                 )
@@ -52,7 +52,7 @@ public class DistributionHelperTest {
                 "{\"files\": [{\"aql\": {\"items.find\": {\"repo\": \"a\", \"path\": \"b\", \"name\": \"c\"}}, \"target\": \"a/b/x\"}]}",
                 Collections.singletonList(
                         new DistributionSpecComponent(
-                                "items.find({\"repo\":\"a\",\"path\":\"b\",\"name\":\"c\"}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")",
+                                "items.find({\"repo\":\"a\",\"path\":\"b\",\"name\":\"c\"}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")",
                                 Collections.singletonList(new PathMapping("^$", "a/b/x")),
                                 new ArrayList<>())
                 )
--- src/test/java/org/jfrog/filespecs/entities/FileSpecTest.java
+++ src/test/java/org/jfrog/filespecs/entities/FileSpecTest.java
@@ -19,31 +19,31 @@ public class FileSpecTest {
     private static Object[][] toSearchAqlProvider() {
         return new Object[][]{{
                 "{\"files\": [{\"pattern\": \"repo-local\"}]}",
-                Collections.singletonList("items.find({\"$or\":[{\"$and\":[{\"repo\":\"repo-local\",\"path\":{\"$match\":\"*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")")
+                Collections.singletonList("items.find({\"$or\":[{\"$and\":[{\"repo\":\"repo-local\",\"path\":{\"$match\":\"*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")")
         }, {
                 "{\"files\": [{\"pattern\": \"repo-w*ldcard\"}]}",
-                Collections.singletonList("items.find({\"$or\":[{\"$and\":[{\"repo\":{\"$match\":\"repo-w*\"},\"path\":{\"$match\":\"*\"},\"name\":{\"$match\":\"*ldcard\"}}]},{\"$and\":[{\"repo\":{\"$match\":\"repo-w*ldcard\"},\"path\":{\"$match\":\"*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")")
+                Collections.singletonList("items.find({\"$or\":[{\"$and\":[{\"repo\":{\"$match\":\"repo-w*\"},\"path\":{\"$match\":\"*\"},\"name\":{\"$match\":\"*ldcard\"}}]},{\"$and\":[{\"repo\":{\"$match\":\"repo-w*ldcard\"},\"path\":{\"$match\":\"*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")")
         }, {
                 "{\"files\": [{\"pattern\": \"repo-local2/a*b*c/dd/\"}]}",
-                Collections.singletonList("items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"repo-local2\",\"path\":{\"$match\":\"a*b*c/dd\"},\"name\":{\"$match\":\"*\"}}]},{\"$and\":[{\"repo\":\"repo-local2\",\"path\":{\"$match\":\"a*b*c/dd/*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")")
+                Collections.singletonList("items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"repo-local2\",\"path\":{\"$match\":\"a*b*c/dd\"},\"name\":{\"$match\":\"*\"}}]},{\"$and\":[{\"repo\":\"repo-local2\",\"path\":{\"$match\":\"a*b*c/dd/*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")")
         }, {
                 "{\"files\": [{\"pattern\": \"repo-local*/a*b*c/dd/\"}]}",
-                Collections.singletonList("items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":{\"$match\":\"repo-local*\"},\"path\":{\"$match\":\"*a*b*c/dd\"},\"name\":{\"$match\":\"*\"}}]},{\"$and\":[{\"repo\":{\"$match\":\"repo-local*\"},\"path\":{\"$match\":\"*a*b*c/dd/*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")")
+                Collections.singletonList("items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":{\"$match\":\"repo-local*\"},\"path\":{\"$match\":\"*a*b*c/dd\"},\"name\":{\"$match\":\"*\"}}]},{\"$and\":[{\"repo\":{\"$match\":\"repo-local*\"},\"path\":{\"$match\":\"*a*b*c/dd/*\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")")
         }, {
                 "{\"files\": [{\"pattern\": \"repo-local\", \"recursive\": \"false\"}]}",
-                Collections.singletonList("items.find({\"$or\":[{\"$and\":[{\"repo\":\"repo-local\",\"path\":\".\",\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")")
+                Collections.singletonList("items.find({\"$or\":[{\"$and\":[{\"repo\":\"repo-local\",\"path\":\".\",\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")")
         }, {
                 "{\"files\": [{\"pattern\": \"*repo-local\", \"recursive\": \"false\"}]}",
-                Collections.singletonList("items.find({\"$or\":[{\"$and\":[{\"repo\":{\"$match\":\"*\"},\"path\":\".\",\"name\":{\"$match\":\"*repo-local\"}}]},{\"$and\":[{\"repo\":{\"$match\":\"*repo-local\"},\"path\":\".\",\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")")
+                Collections.singletonList("items.find({\"$or\":[{\"$and\":[{\"repo\":{\"$match\":\"*\"},\"path\":\".\",\"name\":{\"$match\":\"*repo-local\"}}]},{\"$and\":[{\"repo\":{\"$match\":\"*repo-local\"},\"path\":\".\",\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")")
         }, {
                 "{\"files\": [{\"pattern\": \"repo-local2/a*b*c/dd/\", \"recursive\": \"false\"}, {\"pattern\": \"*/a*b*c/dd/\", \"recursive\": \"false\"}]}",
                 Arrays.asList(
-                        "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"repo-local2\",\"path\":{\"$match\":\"a*b*c/dd\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")",
-                        "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":{\"$match\":\"*\"},\"path\":{\"$match\":\"*a*b*c/dd\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")"
+                        "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":\"repo-local2\",\"path\":{\"$match\":\"a*b*c/dd\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")",
+                        "items.find({\"path\":{\"$ne\":\".\"},\"$or\":[{\"$and\":[{\"repo\":{\"$match\":\"*\"},\"path\":{\"$match\":\"*a*b*c/dd\"},\"name\":{\"$match\":\"*\"}}]}]}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")"
                 )
         }, {
                 "{\"files\": [{\"aql\": {\"items.find\": {\"repo\": \"a\", \"path\": \"b\", \"name\": \"c\"}}}]}",
-                Collections.singletonList("items.find({\"repo\":\"a\",\"path\":\"b\",\"name\":\"c\"}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"property\")")
+                Collections.singletonList("items.find({\"repo\":\"a\",\"path\":\"b\",\"name\":\"c\"}).include(\"name\",\"repo\",\"path\",\"actual_md5\",\"actual_sha1\",\"size\",\"type\",\"modified\",\"created\",\"property\")")
         }
         };
     }
