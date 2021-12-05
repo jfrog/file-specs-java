f42d10ff2e010210bcbc47512226786246ba4129
Eric Bariaux <375613+ebariaux@users.noreply.github.com>
2021-12-05 16:55:44 +0100

    Update apache commons-lang (2) to current commons-lang3 (#9)

--- src/main/java/org/jfrog/filespecs/aql/AqlBuildingUtils.java
+++ src/main/java/org/jfrog/filespecs/aql/AqlBuildingUtils.java
@@ -1,7 +1,7 @@
 package org.jfrog.filespecs.aql;
 
-import org.apache.commons.lang.ArrayUtils;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.util.ArrayList;
 import java.util.Arrays;
